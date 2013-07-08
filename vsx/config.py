7d96496a761613bd355257fc1b6b9ef5710c265d
Simone <simone@galliera.it>
2013-07-08 18:05:41 +0200

    vsx: added command vsx mask show. Added the group mask feature.
    Now it's possible to mask also a server that is not running the instance.

--- vsx/config.py
+++ vsx/config.py
@@ -3,5 +3,5 @@ esm_port = "8443"
 esm_user = "admin"
 esm_password = "********"
 SHELVES = ['96', '1096']
-HOSTS=['mirrina', 'church.ceed', 'pong', 'pacman',
+HOSTS=['mirrina', 'church', 'pong', 'pacman',
        'tetris', 'tron', 'doom', 'toki']
